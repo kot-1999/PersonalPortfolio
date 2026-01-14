14ab859a6e202dc7e7da1960f53c879ec8dcbb28
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-01-14 11:47:44 +0000

    paths were updated

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,14 +1,14 @@
-export const iconsPathBase = '../assets/icons/'
+export const iconsPathBase = 'assets/icons/'
 export const pngFileExtension = '.png'
-export const logosPathBase = '../assets/logos/'
+export const logosPathBase = 'assets/logos/'
 export const iconExtension = '-icon'
 export const smallLogoExtension = '-logo-small'
 export const largeLogoExtension = '-logo-large'
-export const projectImagesPathBase = '../assets/projectImages/'
+export const projectImagesPathBase = 'assets/projectImages/'
 export const thumbnailExtension = '-thumbnail'
 export const largeImageExtension = '-large'
 export const smallImageExtension = '-small'
-export const libraryPathBase = '../assets/bookCovers/'
+export const libraryPathBase = 'assets/bookCovers/'
 
 export const BOOK_CATEGORY = {
     ALL: 'All',
